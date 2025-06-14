e7f93b6d4f63ec7653465fa0a4c303260478dc54
Muttu93 <rajhubballi@gmail.com>
2025-06-14 07:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,59 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
+import webbrowser
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from datetime import datetime
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+# Use 'data' folder inside the project for sections
+DATA_FOLDER = 'data'
 SECTIONS = ['Material', 'Store', 'Employee']
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Ensure section folders exist
-for section in SECTIONS:
-    os.makedirs(os.path.join(UPLOAD_FOLDER, section), exist_ok=True)
-
-def list_files(section):
-    folder = os.path.join(app.config['UPLOAD_FOLDER'], section)
-    files = []
-    for filename in os.listdir(folder):
-        if filename.endswith('.pdf'):
-            path = os.path.join(folder, filename)
-            upload_time = datetime.fromtimestamp(os.path.getmtime(path)).strftime('%Y-%m-%d %H:%M:%S')
-            files.append({'name': filename, 'upload_time': upload_time})
-    return files
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    section = request.args.get('section', 'Store')  # Default to Store
-    search_query = request.args.get('query', '').lower()
-    files = list_files(section)
-
-    if search_query:
-        files = [f for f in files if search_query in f['name'].lower()]
-
-    return render_template('index.html', sections=SECTIONS, selected=section, files=files)
+    section = request.args.get('section', 'Material')
+    files = []
+    section_folder = os.path.join(DATA_FOLDER, section)
+    if os.path.exists(section_folder):
+        files = os.listdir(section_folder)
+    files_data = []
+    for file in files:
+        filepath = os.path.join(section_folder, file)
+        if os.path.isfile(filepath):
+            upload_time = datetime.fromtimestamp(os.path.getmtime(filepath))
+            files_data.append({
+                'name': file,
+                'upload_time': upload_time.strftime('%Y-%m-%d %H:%M:%S'),
+                'path': f'{section}/{file}'
+            })
+    return render_template('index.html', sections=SECTIONS, selected_section=section, files=files_data)
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    section = request.form['section']
-    file = request.files['file']
-    if file and file.filename.endswith('.pdf'):
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], section, file.filename)
-        file.save(filepath)
+def upload():
+    section = request.form.get('section', 'Material')
+    file = request.files.get('file')
+    if file and file.filename != '':
+        save_path = os.path.join(DATA_FOLDER, section)
+        os.makedirs(save_path, exist_ok=True)
+        file.save(os.path.join(save_path, file.filename))
     return redirect(url_for('index', section=section))
 
 @app.route('/delete', methods=['POST'])
-def delete_files():
-    section = request.form['section']
-    selected_files = request.form.getlist('selected_files')
-    for filename in selected_files:
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], section, filename)
+def delete():
+    section = request.form.get('section', 'Material')
+    files_to_delete = request.form.getlist('files')
+    section_folder = os.path.join(DATA_FOLDER, section)
+    for file in files_to_delete:
+        filepath = os.path.join(section_folder, file)
         if os.path.exists(filepath):
             os.remove(filepath)
     return redirect(url_for('index', section=section))
 
-@app.route('/view/<section>/<filename>')
-def view_file(section, filename):
-    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], section), filename)
-
-import webbrowser
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import os
-from datetime import datetime
-
-app = Flask(__name__)
-DATA_FOLDER = 'data'
-app.config['DATA_FOLDER'] = DATA_FOLDER
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# your other routes here ...
+@app.route('/view/<path:filename>')
+def view(filename):
+    return send_from_directory(DATA_FOLDER, filename)
 
 if __name__ == '__main__':
-    # Automatically open browser
+    # Auto open browser
     webbrowser.open("http://127.0.0.1:5000")
     app.run(debug=False)
-
-
-# if __name__ == '__main__':
-#    app.run(debug=True)
